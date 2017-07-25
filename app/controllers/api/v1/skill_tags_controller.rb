1475460d52972c34e44ee1bba19ca50c50453887
MihoSasaki <miho.sprit@gmail.com>
2017-07-25 13:24:45 +0900

    add api controller&routes of skill tag of user

---
+++ app/controllers/api/v1/skill_tags_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::SkillTagsController < ApplicationController
+
+  # tag10個目+tagにいいねを押した人を追加
+  def tags_with_follower
+
+  end
+
+  # tag10個目以降
+  def tags
+
+  end
+
+  # userにtagを追加
+  def add
+
+  end
+
+  # userからtagを削除
+  def destroy
+
+  end
+
+  # skill_tagにfollowerを追加
+  def follow_skill
+
+  end
+
+  # skill_tagからfollowerを削除
+  def unfollow_skill
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,22 @@ Rails.application.routes.draw do
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :skill_tags, only: [:index, :show]
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [] do
+            resources :tags, controller: :skill_tags, only: [:destroy] do
+              member do
+                post :add
+                put :follow_skill
+                delete :unfollow_skill
+              end
+              collection do
+                get :tags_with_followers
+                get :tags
+              end
+            end
+      end
+    end
+  end
 end
